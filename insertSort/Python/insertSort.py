d604d85c99cf055c35337cbab2e32d9b789e0d3e
johncanthony <john.claud.anthony@gmail.com>
2016-11-13 00:51:18 -0800

    insertSort implented with unittests

--- insertSort/Python/insertSort.py
+++ insertSort/Python/insertSort.py
@@ -1,4 +1,12 @@
-class insertSort(object):
+class insertSort():
 
-	def run_sort(_list):
-		return [0,1,3]
+	def run_sort(self,_list):
+		for x in range(1,len(_list)):
+			k=x
+			while(k>0):
+				if(_list[k] <  _list[k-1]):
+					q=_list[k]
+					_list[k] = _list[k-1]
+					_list[k-1] = q
+				k=k-1
+		return _list 
--- insertSort/Python/test_insertSort.py
+++ insertSort/Python/test_insertSort.py
@@ -1,12 +1,27 @@
 import unittest
-from insertSort import *
+from insertSort import * 
+
 
 class TestinsertSort(unittest.TestCase):
 
-	def test_insertSort(self):
-		inVal=[0,3,1]
-		self.assertEqual(insertSort.run_sort(inVal),[0,1,3])
+	test_1 = [[0,3,1],[0,1,3]]
+	test_2 = [[5,2,6,1,1],[1,2,1,5,6]]
+	test_3 = [[-1,3,5,99,0,2,4,4],[-1,0,2,3,4,4,5,99]]
 
+	def test_insertSort(self):
+		sortObj=insertSort()
+		self.assertEqual(sortObj.run_sort(self.test_1[0]),self.test_1[1])
+
+	def test_insertSort2(self):
+		sortObj=insertSort()
+		self.assertNotEqual(sortObj.run_sort(self.test_2[0]),self.test_2[1])
+
+	def test_insertSort3(self):
+		sortObj=insertSort()
+		self.assertEqual(sortObj.run_sort(self.test_3[0]),self.test_3[1])
+
+
+		
 if __name__ == '__main__':
 	unittest.main()
 	
