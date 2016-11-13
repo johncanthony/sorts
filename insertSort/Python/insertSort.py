0aa719d849a038415eb49f8b92b6a26324b78d0c
johncanthony <john.claud.anthony@gmail.com>
2016-11-13 01:07:46 -0800

    Added docstring comments

--- insertSort/Python/insertSort.py
+++ insertSort/Python/insertSort.py
@@ -1,6 +1,18 @@
+###########################################################
+#  insertSort()
+#
+#  johncanthony  11.13.16
+#
+#  Desc:
+#	Implementaiton of insertSort for algorithms study
+#
+###########################################################
+
+
 class insertSort():
 
 	def run_sort(self,_list):
+		""" Sort a list of integers and return the sorted list """
 		for x in range(1,len(_list)):
 			k=x
 			while(k>0):
--- insertSort/Python/test_insertSort.py
+++ insertSort/Python/test_insertSort.py
@@ -9,14 +9,17 @@ class TestinsertSort(unittest.TestCase):
 	test_3 = [[-1,3,5,99,0,2,4,4],[-1,0,2,3,4,4,5,99]]
 
 	def test_insertSort(self):
+		"""Provide a length 3 list object populated with positive integers and check for correct sort """
 		sortObj=insertSort()
 		self.assertEqual(sortObj.run_sort(self.test_1[0]),self.test_1[1])
-
+		
 	def test_insertSort2(self):
+		"""Provide a length 5 list object populated with positive integers and prove correct sort through contradiction  """
 		sortObj=insertSort()
 		self.assertNotEqual(sortObj.run_sort(self.test_2[0]),self.test_2[1])
 
 	def test_insertSort3(self):
+		""" Provide a length 8 list object populated with  positive and negative integers and check for correct sort """
 		sortObj=insertSort()
 		self.assertEqual(sortObj.run_sort(self.test_3[0]),self.test_3[1])
 
