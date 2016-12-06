ea03e60b111423fb2c88a2ea4034560ea7318d51
John Anthony <anthonyjo@anthonyjo-macbookpro2.roam.corp.google.com>
2016-12-06 12:11:49 -0800

    Clean up pointer_shuffle.py

--- PointerShuffle/pointer_shuffle.py
+++ PointerShuffle/pointer_shuffle.py
@@ -1,36 +1,28 @@
 import random
 
-pointerList=[0,1,2,3,4,5,6,7,8,9]
-pointerList.reverse()
+end=random.randint(3,20)
 
 
-print(pointerList[0])
-lastPiece=pointerList.pop()
-
-print(lastPiece)
-print(pointerList)
-
-totalLength=len(pointerList)
-midPoint=int(len(pointerList)/2)
-
-left=pointerList[:midPoint]
-
-index=0
-right=pointerList[midPoint:]
-random.shuffle(right)
-random.shuffle(left)
-
-index=0
-
-print(left)
-print(right)
-
-index=0
-shuffled=[0]*(totalLength+1)
-print(shuffled)
+#print(pointerList[0])
 
 
-while(len(left) or len(right)):
+def cycle_shuffle(_len):
+
+
+    index=0
+    shuffled=[0]*(_len)
+    pointerList=range(0,_len)
+    pointerList.reverse()
+    lastPiece=pointerList.pop() 
+
+    midPoint=int((_len-1)/2)
+
+    left=pointerList[:midPoint]
+    right=pointerList[midPoint:]
+    random.shuffle(right)
+    random.shuffle(left)
+
+    while(len(left) or len(right)):
 
 	if(len(right)):
 		shuffled[index]=right.pop()
@@ -38,10 +30,42 @@ while(len(left) or len(right)):
 
 	if(len(left)):
 		shuffled[index]=left.pop()
-		print(shuffled[index])
 		index = shuffled[index]
 
 	
-shuffled[index]=lastPiece
-print(shuffled)
-			
+    shuffled[index]=lastPiece
+    
+    return shuffled
+
+
+
+
+
+
+
+
+x = -99
+
+
+while(x==-99):
+
+    print("Enter any positive integer greater than 2 (Enter -1 for a random):")
+    x = input()
+
+    if(not (isinstance(x,int))):
+        print("Err: Please input an integer")
+        x=-99
+        continue
+    
+    if(x==-1):
+        x=random.randint(2,20)
+    
+
+    if(x<2):
+        print("Err: Please input a number greater than 2")
+        x=-99
+        continue
+
+
+    shuf = cycle_shuffle(x)
+    print(shuf)
